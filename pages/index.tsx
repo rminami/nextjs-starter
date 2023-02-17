1730c32baf9c6e692b828b53c69bded334081eeb
Ryosuke Minami <ryosukem13@gmail.com>
2023-02-17 16:38:27 +0900

    set up postcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p>Hello world!</p>
+        <div className="max-w-4xl mx-auto px-8 py-4 w-full">
+          <p className="text-xl">Hello world!</p>
+        </div>
       </main>
     </>
   );
